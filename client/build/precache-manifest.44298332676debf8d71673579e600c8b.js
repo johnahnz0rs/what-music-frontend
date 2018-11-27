a018cc33e1dea757f3648e4c97e0975763deb515
John Ahn <john@launchpartner.co>
2018-11-26 23:57:31 -0800

    build

---
+++ client/build/precache-manifest.44298332676debf8d71673579e600c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58ef49e9712170cd8ef54d81067fd9eb",
+    "url": "/static/media/background.58ef49e9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "280b5e829e8afc852c0a",
+    "url": "/static/js/main.280b5e82.chunk.js"
+  },
+  {
+    "revision": "8b42a4be9cd5779ebfa3",
+    "url": "/static/js/1.8b42a4be.chunk.js"
+  },
+  {
+    "revision": "280b5e829e8afc852c0a",
+    "url": "/static/css/main.301262ae.chunk.css"
+  },
+  {
+    "revision": "5e7f3b1b1889239e2bf59c89fc97ebc8",
+    "url": "/index.html"
+  }
+];
